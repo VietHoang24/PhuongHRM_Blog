0eedfa4d460e002811579295505d743ca72886ff
Leo Nguyen <quang.nguyen@savvycomsoftware.com>
2022-09-23 16:10:38 +0700

    add admin page

---
+++ data/headerNavLinks.js
@@ -0,0 +1,9 @@
+const headerNavLinks = [
+  { href: '/blog', title: 'Blog' },
+  { href: '/tags', title: 'Tags' },
+  { href: '/projects', title: 'Projects' },
+  { href: '/about', title: 'About' }, 
+  { href: '/admin/dashboard', title: 'Admin' },
+]
+
+export default headerNavLinks
